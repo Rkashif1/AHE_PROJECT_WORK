28417e3c8a8e62235a60577889b096ad01b111e4
Rkashif1 <kashifr563@gmail.com>
2024-12-26 19:46:56 +0100

    Files Added

---
+++ Final_Project_Files/Vitis_Implementation/logs/builder.py
@@ -0,0 +1,8 @@
+# 2024-12-26T17:05:09.164283100
+import vitis
+
+client = vitis.create_client()
+client.set_workspace(path="D:/MYGIT/MyRepoData/HealthMonitorSystem/VitisCode")
+
+vitis.dispose()
+
---
+++ Final_Project_Files/Vitis_Implementation/logs/builder_10008.py
@@ -0,0 +1,8 @@
+# 2024-12-26T17:01:21.564824300
+import vitis
+
+client = vitis.create_client()
+client.set_workspace(path="D:/MYGIT/MyRepoData/HealthMonitorSystem/VitisCode")
+
+vitis.dispose()
+
